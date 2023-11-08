32a55e410b1054c78242068c1c229aa5993c5376
jacksonlmp <jacksonlmp17.jl@gmail.com>
2023-11-08 20:57:00 -0300

    Atributos de usuário

---
+++ lib/models/user_atributes.dart
@@ -0,0 +1,12 @@
+class UserAtributes {
+  final String id;
+  final String name;
+  final String email;
+
+  const UserAtributes ({
+    required this.id,
+    required this.name,
+    required this.email
+  });
+  
+}
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -12,13 +12,22 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
 
-    print('AuthPage');
-    print(formData.email);
+      if (formData.isLogin) {
+        //Login
 
-    setState(() => _isLoading = false);
+      } else {
+        //Signup
+      }
+    } catch(error) {
+      //Tratar erro
+    } finally {
+      setState(() => _isLoading = false);
+    }
+
   }
 
   @override
