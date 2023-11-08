265b69726a319bfba04a651f54e4cf288b3da8fc
jacksonlmp <jacksonlmp17.jl@gmail.com>
2023-11-08 20:41:24 -0300

    Mudança de widget e implementação de validações

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,43 +1,106 @@
+import 'package:desafio_flutter_capyba/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
-  const AuthForm({super.key});
+class AuthForm extends StatefulWidget {
+  final void Function(AuthFormData) onSubmit;
+
+  const AuthForm({super.key, required this.onSubmit});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) return;
+
+    widget.onSubmit(_formData);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      //margin: const EdgeInsets.all(16),
+      margin: const EdgeInsets.all(16),
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Nome'),
+              const Text(
+                'Desafio\nCapyba',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 133, 236, 137),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50),
               ),
+              if (_formData.isSignUp)
+                TextFormField(
+                  key: const ValueKey('name'),
+                  initialValue: _formData.name,
+                  onChanged: (name) => _formData.name = name,
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                  validator: (_name) {
+                    final name = _name ?? '';
+                    if (name.trim().length < 5) {
+                      return 'Nome deve possuir no mínimo 5 caracteres';
+                    }
+                    return null;
+                  },
+                ),
               TextFormField(
+                key: const ValueKey('email'),
+                initialValue: _formData.email,
+                onChanged: (email) => _formData.name = email,
                 decoration: const InputDecoration(labelText: 'E-mail'),
+                validator: (_email) {
+                  final email = _email ?? '';
+                  if (!email.contains('@')) {
+                    return 'E-mail informado não é válido.';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 obscureText: true,
+                key: const ValueKey('password'),
+                initialValue: _formData.password,
+                onChanged: (password) => _formData.password = password,
                 decoration: const InputDecoration(labelText: 'Senha'),
+                validator: (_password) {
+                  final password = _password ?? '';
+                  if (password.length < 6) {
+                    return 'Senha deve possuir no mínimo 6 digitos';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: 12),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: _submit,
                 style: const ButtonStyle(
                     backgroundColor: MaterialStatePropertyAll(
                         Color.fromARGB(255, 133, 236, 137))),
-                child: const Text(
-                  'Entrar',
-                  style: TextStyle(color: Colors.white),
+                child: Text(
+                  _formData.isLogin ? 'Entrar' : 'Cadastrar',
+                  style: const TextStyle(color: Colors.white),
                 ),
               ),
               TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Criar uma nova conta',
-                    style: TextStyle(color: Colors.green),
+                  onPressed: () {
+                    setState(() {
+                      _formData.toggleAuthMode();
+                    });
+                  },
+                  child: Text(
+                    _formData.isLogin
+                        ? 'Criar uma nova conta?'
+                        : 'Já possui conta?',
+                    style: const TextStyle(color: Colors.green),
                   ))
             ],
           ),
