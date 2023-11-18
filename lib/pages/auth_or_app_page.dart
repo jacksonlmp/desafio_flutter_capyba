1c73d6348fed79a70527b3424d6e76273adcf373
jacksonlmp <jacksonlmp17.jl@gmail.com>
2023-11-17 23:34:36 -0300

    Tela de Home e de transição de página

---
+++ lib/pages/auth_or_app_page.dart
@@ -0,0 +1,31 @@
+import 'package:desafio_flutter_capyba/core/models/user_atributes.dart';
+import 'package:desafio_flutter_capyba/core/services/auth/auth_mock_service.dart';
+import 'package:desafio_flutter_capyba/pages/auth_page.dart';
+import 'package:desafio_flutter_capyba/pages/home_page.dart';
+import 'package:desafio_flutter_capyba/pages/loading_page.dart';
+import 'package:flutter/material.dart';
+
+class AuthOrAppPage extends StatefulWidget {
+  const AuthOrAppPage({super.key});
+
+  @override
+  State<AuthOrAppPage> createState() => _AuthOrAppPageState();
+}
+
+class _AuthOrAppPageState extends State<AuthOrAppPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<UserAtributes?>(
+        stream: AuthMockService().userChanges,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingPage();
+          } else {
+            return snapshot.hasData ? const HomePage() : const AuthPage();
+          }
+        },
+      ),
+    );
+  }
+}
